b6856c9ada9761084ad65de25e862624ab26d813
esejjec <38010954+esejjec@users.noreply.github.com>
2021-11-28 18:53:12 -0500

    Add files via upload

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS tSeriesDB;
+USE tSeriesDB;
+
+drop table thingData;
+CREATE TABLE IF NOT EXISTS thingData(
+id INT (11),
+topic VARCHAR (1024),
+payload VARCHAR (2048),
+timestamp VARCHAR (15),
+deleted BINARY (1)
+);
+
