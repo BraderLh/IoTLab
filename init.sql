d367cfd3350c911a14a6468cc9201769b12de523
alrus2797 <alrus2797@gmail.com>
2021-11-28 23:49:30 -0500

    Update configuration

--- init.sql
+++ init.sql
@@ -1,7 +1,9 @@
+CREATE USER 'root'@'%' IDENTIFIED BY 'Pass_word1'; 
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' with grant OPTION;
+flush privileges;
 CREATE DATABASE IF NOT EXISTS tSeriesDB;
 USE tSeriesDB;
 
-drop table thingData;
 CREATE TABLE IF NOT EXISTS thingData(
 id INT (11),
 topic VARCHAR (1024),
